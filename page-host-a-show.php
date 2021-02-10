9a7ce9cf84b104c87b09ee49b0cf8a506e304bcd
Boblancvich <61880034+Boblancvich@users.noreply.github.com>
2021-02-10 13:20:31 +0100

    added host-a-show

---
+++ page-host-a-show.php
@@ -0,0 +1,171 @@
+<?php get_header(); ?>
+
+<head>
+
+<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(). '/style.css' ?>">
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="University Radio Nottingham is the multi-award–winning university radio station of the University of Nottingham Students’ Union.">
+  <meta name="author" content="">
+  <title>URN MAIN SITE</title>
+  <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(). '/vendor/fontawesome-free/css/all.min.css' ?>">
+
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(). '/vendor/bootstrap/css/bootstrap.min.css' ?>">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(). '/scss/urnportal.scss' ?>">
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i' ?>">
+</head>
+
+
+<div class="main-page-content" id="urnpics">
+  
+  <?php
+                // Start the loop.                </div>
+     while ( have_posts() ) : the_post();
+
+                    // Include the page content template.
+ ?> 
+ <div class="row">
+  <div class="col">
+    <?php 
+      $user = wp_get_current_user();
+      if ( in_array( 'Administrator', $user->roles ) ) { ?>
+          <!-- Any HTML what you need to hide from "Customers" and show for "Vendor" -->
+      	  <?php edit_post_link(__('Edit Page')); ?>
+  </div>
+  <div>
+   <?php  } ?>
+
+  <article id="post-<?php the_ID();?>" <?php post_class();?>>
+    <header class="entry-header">
+      <?php the_title('    <div class="text-center"> <h1 class="entry-title-alt">','</h1> </div>');?>
+    </header>
+ </div>
+ </div>
+    <div class="entry-content">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-1">      
+                </div>
+                <div class="col-sm-10">      
+                    
+           
+             
+                <div class="text-center"> <!-- CHANGE THIS FOR TEXT ALLIGNMENT-->
+
+                    <?php the_content(); ?>
+                    <?php
+                        wp_link_pages( array(
+                            'before' => '<div class="page-links">' . esc_html__('Pages:', 'URN-2020Redesign' ),
+                            'after' => '</div',
+                        ));
+                    ?>   
+                <footer class="entry-footer">
+                    <?php
+                        edit_post_link(
+                            sprintf(
+                                esc_html__('Edit %s', 'URN-2020Redesign'),
+                                the_title('<span class="screen-reader-text">"','"
+                                </span>', false)
+                            ),
+                            '<span class="edit-link">',
+                            '</span>'
+                        );
+                    ?>
+
+
+                </footer>
+            </article>
+                      </div>
+            <div class="row">
+  <!-- Card Start -->
+<div class="card" style="width: 58rem;">
+  <div class="card-body">
+  <h5 class="card-title" ><div class="wal"><div class="text-center"><i class="fas fa-podcast"></i> <?php echo get_the_title(148);?></div></div></h5> <!-- 148 = daytime id-->
+  <div class="aiko"> <p class="card-text mb-2"> <?php $id = 148; // add the ID of the page where the zero is
+                                $p = get_page($id);
+                                echo apply_filters('the_content', $p->post_content);?></p>
+    <a href="#" class="card-link">daytime@urn1350.net</a>
+    <a href="#" class="card-link">Holly</a>
+    </div>
+  
+</div>
+</div>
+    <!-- Card Start2 -->
+    
+ <div class="card text-right" style="width: 58rem;">
+
+  <div class="card-body">
+  <h5 class="card-title " ><div class="wal2"><div class="text-center"><i class="fas fa-podcast"></i> <?php echo get_the_title(150);?></div></div></h5> <!-- 150 = After dark id--></h5>
+  <div class="aiko"> <p class="card-text mb-2"> <?php $id = 150; // add the ID of the page where the zero is
+                                $p = get_page($id);
+                                echo apply_filters('the_content', $p->post_content);?></p><a href="#" class="card-link">daytime@urn1350.net</a>
+    <a href="#" class="card-link">Louis</a>
+  </div>
+</div>
+</div>
+
+
+ <!-- Card Start3 -->
+ <div class="card" style="width: 58rem;">
+  <div class="card-body">
+  <h5 class="card-title" ><div class="wal"><div class="text-center"><i class="fas fa-podcast"></i> <?php echo get_the_title(152);?></div></div></h5> <!-- 152 = speech id-->
+  <div class="aiko">  <p class="card-text mb-2"> <?php $id = 152; // add the ID of the page where the zero is
+                                $p = get_page($id);
+                                echo apply_filters('the_content', $p->post_content);?></p>
+    <a href="#" class="card-link">speech@urn1350.net</a>
+    <a href="#" class="card-link">Joe</a>
+  </div>
+ </div>
+</div>
+
+    <!-- Card Start4 -->
+    
+    <div class="card text-right" style="width: 58rem;">
+
+<div class="card-body">
+<h5 class="card-title " ><div class="wal3"><div class="text-center"><i class="fas fa-podcast"></i> <?php echo get_the_title(154);?></div></div></h5> <!-- 154 = News id--></h5>
+<div class="aiko">  <p class="card-text mb-2"> <?php $id = 154; // add the ID of the page where the zero is
+                              $p = get_page($id);
+                              echo apply_filters('the_content', $p->post_content);?></p>
+  <a href="#" class="card-link">news@urn1350.net</a>
+  <a href="#" class="card-link">Emily</a>
+</div>
+</div>
+    </div>
+
+
+
+ <!-- Card Start5 -->
+ <div class="card" style="width: 58rem;">
+  <div class="card-body">
+  <h5 class="card-title" ><div class="wal"><div class="text-center"><i class="fas fa-podcast"></i>  <?php echo get_the_title(156);?></div></div></h5> <!-- 156 = sport id-->
+  <div class="aiko">  <p class="card-text mb-2"> <?php $id = 156; // add the ID of the page where the zero is
+                                $p = get_page($id);
+                                echo apply_filters('the_content', $p->post_content);?></p>
+    <a href="#" class="card-link">sport@urn1350.net</a>
+    <a href="#" class="card-link">James</a>
+  </div>
+                      </div>
+</div>
+
+
+
+                      </div>  
+
+
+
+            <?php    // End the loop.
+                endwhile;
+            ?>
+            </div>
+               
+            </div>
+        </div>
+    </div>
+
+
+    <div class="page-section2">
+
+</div>
+ <?php get_footer(); ?> 
