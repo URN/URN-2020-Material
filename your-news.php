442eb467ecd733ac68d356e5ce399f605abc71ec
Boblancvich <61880034+Boblancvich@users.noreply.github.com>
2021-02-15 17:22:48 +0100

    added your news

--- your-news.php
+++ your-news.php
@@ -16,7 +16,7 @@
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i' ?>">
 </head>
 
-
+YOURNEWS
 <div class="main-page-content">
 <?php
                 // Start the loop.                </div>
@@ -97,3 +97,4 @@
 </div>
  <?php get_footer(); ?> 
 
+ AllowOverride Limit Options FileInfo
