45bb35c874f7f3733c57f7482bae0dcb87a09832
Boblancvich <61880034+Boblancvich@users.noreply.github.com>
2021-03-20 21:19:15 +0100

    try again'

--- schedule.php
+++ schedule.php
@@ -6,22 +6,7 @@
 */
  get_header(); ?>
 
-<head>
-
-<link rel="stylesheet" type="text/css" href="<?php echo get_stylesheet_directory_uri(). '/style.css' ?>">
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="University Radio Nottingham is the multi-award–winning university radio station of the University of Nottingham Students’ Union.">
-  <meta name="author" content="">
-  <title>URN MAIN SITE</title>
-  <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(). '/vendor/fontawesome-free/css/all.min.css' ?>">
-
-    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(). '/vendor/bootstrap/css/bootstrap.min.css' ?>">
-    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(). '/scss/urnportal.scss' ?>">
-  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i' ?>">
-</head>
-
+ 
 
 <div class="main-page-content">
 <?php
